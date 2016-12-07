a56e5f9aa11bce9ab19fab28797bfa8b49f5f16d
charles-penunia <cpenuniatcmc@gmail.com>
2016-12-06 21:31:18 -0500

    Update app id
    My mistake

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@
 /**
  * App ID for the skill
  */
-var APP_ID =  "amzn1.ask.skill.345c7f5a-160f-4a3e-99c8-ced29f7ede90"; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+var APP_ID =  "amzn1.ask.skill.3498e299-c62e-4251-bdcd-42925085447d"; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
 
 
 /**
